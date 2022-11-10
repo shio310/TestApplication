a0734dbb75a9fb3acae3dd6357d34aef511e1c35
TakuShiozawa <taku_0310@hotmail.com>
2022-11-10 17:44:24 +0900

    あえ

--- TestApplication/test1.cs
+++ TestApplication/test1.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestApplication
 {
+    /// <summary>
+    /// TEst1 クラス
+    /// </summary>
     internal class test1
     {
+        public test1()
+        {
+            Debug.Write("abc");
+        }
     }
 }
